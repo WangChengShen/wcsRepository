2747873b1376fb2b99fefd9912428ab1a2e7869c
承申 王 <805967524@qq.com>
2020-04-09 23:23:25 +0800

    添加IdentityServier4的使用

---
+++ Wcs.MicroService.ServiceInstance/Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Wcs.MicroService.ServiceInstance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private IConfiguration _iConfiguration;
+        public TestController(IConfiguration iConfiguration)
+        {
+            _iConfiguration = iConfiguration;
+        }
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            Console.WriteLine($"This is TestController  {this._iConfiguration["port"]} Invoke");
+
+            return new JsonResult(new
+            {
+                message = "This is TestControllerIndex",
+                Port = this._iConfiguration["port"],
+                Time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")
+            });
+        }
+
+        /// <summary>
+        /// 打了Authorize特性，需要验证token
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet]
+        [Route("IndexA")]
+        public IActionResult IndexA()
+        {
+            Console.WriteLine($"This is TestController  {this._iConfiguration["port"]} Invoke");
+
+            return new JsonResult(new
+            {
+                message = "This is TestControllerIndex",
+                Port = this._iConfiguration["port"],
+                Time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")
+            });
+        }
+    }
+}
---
+++ Wcs.MicroService.ServiceInstance/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Wcs.MicroService.ServiceInstance
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://127.0.0.1:8001"; //配置鉴权中心的站点，因为要去拿公钥
+                    options.ApiName = "UserApi"; //作用域
+                    options.RequireHttpsMetadata = false;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();//添加鉴权
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
