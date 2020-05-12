c40cb2dac61a93e5570cd0f8d310c5e12a099c43
承申 王 <805967524@qq.com>
2020-05-12 22:03:05 +0800

    多线程调度实现双色球

--- MultiThread/Form1.Designer.cs
+++ MultiThread/Form1.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.gb1 = new System.Windows.Forms.GroupBox();
-            this.lbRed1 = new System.Windows.Forms.Label();
-            this.lbRed2 = new System.Windows.Forms.Label();
-            this.lbRed3 = new System.Windows.Forms.Label();
-            this.lbRed4 = new System.Windows.Forms.Label();
-            this.lbRed5 = new System.Windows.Forms.Label();
-            this.lbRed6 = new System.Windows.Forms.Label();
-            this.lbBlue = new System.Windows.Forms.Label();
-            this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbBlue = new System.Windows.Forms.Label();
+            this.lbRed6 = new System.Windows.Forms.Label();
+            this.lbRed5 = new System.Windows.Forms.Label();
+            this.lbRed4 = new System.Windows.Forms.Label();
+            this.lbRed3 = new System.Windows.Forms.Label();
+            this.lbRed2 = new System.Windows.Forms.Label();
+            this.lbRed1 = new System.Windows.Forms.Label();
             this.gb1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,125 +52,137 @@
             this.gb1.Controls.Add(this.lbRed3);
             this.gb1.Controls.Add(this.lbRed2);
             this.gb1.Controls.Add(this.lbRed1);
-            this.gb1.Location = new System.Drawing.Point(113, 79);
+            this.gb1.Location = new System.Drawing.Point(57, 44);
+            this.gb1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gb1.Name = "gb1";
-            this.gb1.Size = new System.Drawing.Size(537, 318);
+            this.gb1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gb1.Size = new System.Drawing.Size(498, 284);
             this.gb1.TabIndex = 0;
             this.gb1.TabStop = false;
             this.gb1.Text = "双色球号码";
             // 
-            // lbRed1
-            // 
-            this.lbRed1.AutoSize = true;
-            this.lbRed1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbRed1.ForeColor = System.Drawing.Color.Red;
-            this.lbRed1.Location = new System.Drawing.Point(104, 123);
-            this.lbRed1.Name = "lbRed1";
-            this.lbRed1.Size = new System.Drawing.Size(40, 28);
-            this.lbRed1.TabIndex = 0;
-            this.lbRed1.Text = "00";
-            this.lbRed1.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lbRed2
-            // 
-            this.lbRed2.AutoSize = true;
-            this.lbRed2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbRed2.ForeColor = System.Drawing.Color.Red;
-            this.lbRed2.Location = new System.Drawing.Point(150, 123);
-            this.lbRed2.Name = "lbRed2";
-            this.lbRed2.Size = new System.Drawing.Size(40, 28);
-            this.lbRed2.TabIndex = 0;
-            this.lbRed2.Text = "00";
-            this.lbRed2.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lbRed3
-            // 
-            this.lbRed3.AutoSize = true;
-            this.lbRed3.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbRed3.ForeColor = System.Drawing.Color.Red;
-            this.lbRed3.Location = new System.Drawing.Point(196, 123);
-            this.lbRed3.Name = "lbRed3";
-            this.lbRed3.Size = new System.Drawing.Size(40, 28);
-            this.lbRed3.TabIndex = 0;
-            this.lbRed3.Text = "00";
-            this.lbRed3.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lbRed4
-            // 
-            this.lbRed4.AutoSize = true;
-            this.lbRed4.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbRed4.ForeColor = System.Drawing.Color.Red;
-            this.lbRed4.Location = new System.Drawing.Point(242, 123);
-            this.lbRed4.Name = "lbRed4";
-            this.lbRed4.Size = new System.Drawing.Size(40, 28);
-            this.lbRed4.TabIndex = 0;
-            this.lbRed4.Text = "00";
-            this.lbRed4.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lbRed5
-            // 
-            this.lbRed5.AutoSize = true;
-            this.lbRed5.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbRed5.ForeColor = System.Drawing.Color.Red;
-            this.lbRed5.Location = new System.Drawing.Point(288, 123);
-            this.lbRed5.Name = "lbRed5";
-            this.lbRed5.Size = new System.Drawing.Size(40, 28);
-            this.lbRed5.TabIndex = 0;
-            this.lbRed5.Text = "00";
-            this.lbRed5.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lbRed6
-            // 
-            this.lbRed6.AutoSize = true;
-            this.lbRed6.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbRed6.ForeColor = System.Drawing.Color.Red;
-            this.lbRed6.Location = new System.Drawing.Point(334, 123);
-            this.lbRed6.Name = "lbRed6";
-            this.lbRed6.Size = new System.Drawing.Size(40, 28);
-            this.lbRed6.TabIndex = 0;
-            this.lbRed6.Text = "00";
-            this.lbRed6.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // lbBlue
-            // 
-            this.lbBlue.AutoSize = true;
-            this.lbBlue.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbBlue.ForeColor = System.Drawing.Color.Blue;
-            this.lbBlue.Location = new System.Drawing.Point(406, 123);
-            this.lbBlue.Name = "lbBlue";
-            this.lbBlue.Size = new System.Drawing.Size(40, 28);
-            this.lbBlue.TabIndex = 0;
-            this.lbBlue.Text = "00";
-            this.lbBlue.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(93, 226);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(137, 38);
-            this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "开始";
-            this.btnStart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(309, 226);
+            this.btnStop.Location = new System.Drawing.Point(236, 170);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(137, 38);
+            this.btnStop.Size = new System.Drawing.Size(103, 30);
             this.btnStop.TabIndex = 1;
             this.btnStop.Text = "结束";
             this.btnStop.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(71, 170);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(103, 30);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开始";
+            this.btnStart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbBlue
+            // 
+            this.lbBlue.AutoSize = true;
+            this.lbBlue.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbBlue.ForeColor = System.Drawing.Color.Blue;
+            this.lbBlue.Location = new System.Drawing.Point(320, 87);
+            this.lbBlue.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbBlue.Name = "lbBlue";
+            this.lbBlue.Size = new System.Drawing.Size(53, 35);
+            this.lbBlue.TabIndex = 0;
+            this.lbBlue.Text = "00";
+            this.lbBlue.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbRed6
+            // 
+            this.lbRed6.AutoSize = true;
+            this.lbRed6.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbRed6.ForeColor = System.Drawing.Color.Red;
+            this.lbRed6.Location = new System.Drawing.Point(260, 87);
+            this.lbRed6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRed6.Name = "lbRed6";
+            this.lbRed6.Size = new System.Drawing.Size(53, 35);
+            this.lbRed6.TabIndex = 0;
+            this.lbRed6.Text = "00";
+            this.lbRed6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbRed5
+            // 
+            this.lbRed5.AutoSize = true;
+            this.lbRed5.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbRed5.ForeColor = System.Drawing.Color.Red;
+            this.lbRed5.Location = new System.Drawing.Point(209, 87);
+            this.lbRed5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRed5.Name = "lbRed5";
+            this.lbRed5.Size = new System.Drawing.Size(53, 35);
+            this.lbRed5.TabIndex = 0;
+            this.lbRed5.Text = "00";
+            this.lbRed5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbRed4
+            // 
+            this.lbRed4.AutoSize = true;
+            this.lbRed4.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbRed4.ForeColor = System.Drawing.Color.Red;
+            this.lbRed4.Location = new System.Drawing.Point(160, 87);
+            this.lbRed4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRed4.Name = "lbRed4";
+            this.lbRed4.Size = new System.Drawing.Size(53, 35);
+            this.lbRed4.TabIndex = 0;
+            this.lbRed4.Text = "00";
+            this.lbRed4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbRed3
+            // 
+            this.lbRed3.AutoSize = true;
+            this.lbRed3.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbRed3.ForeColor = System.Drawing.Color.Red;
+            this.lbRed3.Location = new System.Drawing.Point(113, 87);
+            this.lbRed3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRed3.Name = "lbRed3";
+            this.lbRed3.Size = new System.Drawing.Size(53, 35);
+            this.lbRed3.TabIndex = 0;
+            this.lbRed3.Text = "00";
+            this.lbRed3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbRed2
+            // 
+            this.lbRed2.AutoSize = true;
+            this.lbRed2.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbRed2.ForeColor = System.Drawing.Color.Red;
+            this.lbRed2.Location = new System.Drawing.Point(65, 87);
+            this.lbRed2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRed2.Name = "lbRed2";
+            this.lbRed2.Size = new System.Drawing.Size(53, 35);
+            this.lbRed2.TabIndex = 0;
+            this.lbRed2.Text = "00";
+            this.lbRed2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbRed1
+            // 
+            this.lbRed1.AutoSize = true;
+            this.lbRed1.Font = new System.Drawing.Font("宋体", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbRed1.ForeColor = System.Drawing.Color.Red;
+            this.lbRed1.Location = new System.Drawing.Point(17, 87);
+            this.lbRed1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRed1.Name = "lbRed1";
+            this.lbRed1.Size = new System.Drawing.Size(53, 35);
+            this.lbRed1.TabIndex = 0;
+            this.lbRed1.Text = "00";
+            this.lbRed1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(808, 486);
+            this.ClientSize = new System.Drawing.Size(606, 389);
             this.Controls.Add(this.gb1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.gb1.ResumeLayout(false);
