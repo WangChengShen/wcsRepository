e1457b61b70127e68c00bf01c0d3eb4e97cf56b7
承申 王 <805967524@qq.com>
2020-05-25 00:02:45 +0800

    a

---
+++ ApiGateway/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllers();
+
+            /*nuget先引入Ocelot包*/
+            // var config = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+            var config = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+            services.AddOcelot(config);
+              //.AddConsul();//使用Consul,从Consul里面发现服务，nuget安装Ocelot.Provider.Consul
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseOcelot().Wait();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.UseRouting();
+
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+        }
+    }
+}
---
+++ MyServiceB/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MyServiceB.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private IConfiguration configuration;
+        public OrderController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public ActionResult Get()
+        {
+            string url = $"{configuration["ip"]}:{configuration["port"]}";
+
+            return Ok($"Order From MyServiceB[{url}]");
+        }
+    }
+}
