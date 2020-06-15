a7680a3ee7af5619cbe579d55646b209404046d2
承申 王 <805967524@qq.com>
2020-06-15 22:52:33 +0800

    11

---
+++ RedisWebDemo/Controllers/CacheController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisWebDemo.Controllers
+{
+    public class CacheController : Controller
+    {
+        private readonly IDistributedCache distributedCache;
+        public CacheController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+        public IActionResult Index()
+        {
+            distributedCache.SetString("Hello", "Cache");
+            distributedCache.GetString("Hello");
+            return View();
+        }
+    }
+}
---
+++ RedisWebDemo/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RedisWebDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /*Redis在Core里面的使用：
+             1.nuget引入Microsoft.Extensions.Caching.StackExchangeRedis包
+             2.Redis,Sql Server等分布式缓存，都实现的是同一套接口，IDistributedCache,因此使用时注入此接口进行使用
+             */ 
+            services.AddStackExchangeRedisCache(options=> {
+                options.Configuration = "localhost";//主机Ip
+                options.InstanceName = "myRedis";//实例名称
+            });
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
