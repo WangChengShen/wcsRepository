4ff0073ebd84bc3af4151cd8baac6f4bace47632
承申 王 <805967524@qq.com>
2020-08-23 23:51:39 +0800

    爬虫的简单实现

---
+++ Crawler/Program.cs
@@ -0,0 +1,83 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Linq;
+namespace Crawler
+{
+    /// <summary>
+    /// 网络爬虫
+    /// 解析html的一个包HtmlAgilityPack，其原理是根据正则表达式找字符串
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://ke.qq.com/course/list/";
+            string result = RequestUrl(url);
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(result);
+            string pagePath = "/html/body/section[1]/div/div[6]/a[@class='page-btn']";
+            HtmlNodeCollection pageNodes = htmlDocument.DocumentNode.SelectNodes(pagePath);
+            int maxPage = pageNodes.Select(p => Convert.ToInt32(p.InnerText)).Max();
+
+            for (int page = 1; page <= maxPage; page++)
+            {
+                url = $"https://ke.qq.com/course/list?page={page}";
+                getDataByUrl(url);
+            }
+
+            Console.WriteLine($"总共爬取数据{num}条");
+        }
+
+        static int num = 0;
+        static void getDataByUrl(string url)
+        {
+            string result = RequestUrl(url);
+
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(result);
+
+            //xpath可以通过浏览器查看元素，右键选择copy xpath进行复制
+            string xpath = "/html/body/section[1]/div/div[4]/ul/li";
+
+            HtmlNodeCollection htmlNode = htmlDocument.DocumentNode.SelectNodes(xpath);
+             
+            foreach (var itemNodel in htmlNode)
+            {
+                num++;
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(itemNodel.OuterHtml);
+
+                //找当前li下面的a的xpath写法，也可以加一些属性条件，如：  "//*/a[@class='myA']", "//*/a[@name='myA']" 等
+
+                HtmlNode aNote = document.DocumentNode.SelectSingleNode("//*/a");
+                string id = aNote.Attributes["data-id"].Value;
+                Console.WriteLine($"【{num}】课程id:{id}");
+                HtmlNode imgNote = document.DocumentNode.SelectSingleNode("//*/a/img");
+                string imgUrl = imgNote.Attributes["src"].Value;
+                Console.WriteLine($"【{num}】课程图片:{imgUrl}");
+                string title = imgNote.Attributes["title"].Value;
+                Console.WriteLine($"【{num}】课程标题:{title}");
+                Console.WriteLine("#############");
+            }
+        }
+         
+        static string RequestUrl(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Method = HttpMethod.Get;
+                message.RequestUri = new Uri(url);
+
+                var result = client.SendAsync(message);
+                string content = result.Result.Content.ReadAsStringAsync().Result;
+
+                return content;
+            }
+        }
+    }
+}
---
+++ Crawler/Unility/HttpHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace Crawler
+{
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="P"></typeparam>
+        /// <param name="apiUrl"></param>
+        /// <param name="param"></param>
+        /// <param name="serviceCode"></param>
+        /// <returns></returns>
+        public static string PostWebApi<P>(string apiUrl, P param)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(apiUrl);
+            //request.KeepAlive = false;
+            request.Timeout = 300000;
+            request.Method = "post";
+            byte[] postdatabyte = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(param));
+            request.ContentLength = postdatabyte.Length;
+            request.ContentType = "application/json;charset=UTF-8";
+            //request.Headers.Add("appId", AppId);
+
+            Stream stream = request.GetRequestStream();
+            stream.Write(postdatabyte, 0, postdatabyte.Length);
+            stream.Close();
+
+            using (HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse())
+            {
+                Stream myResponseStream = httpWebResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(myResponseStream);
+                string jsonStr = streamReader.ReadToEnd();
+
+                streamReader.Close();
+                myResponseStream.Close();
+                return jsonStr;
+            }
+        }
+
+        public static string PostWebApi2<P>(string apiUrl, P param)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, apiUrl);
+                //request.Content = new FormUrlEncodedContent(dic.Select(s => s));
+
+                request.Content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(param));
+                request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                //request.Headers.Add("appId", AppId);
+                //request.Headers.Add("serviceCode", serviceCode);
+                //request.Content.Headers.Add("Content-Signature", $"HMAC-SHA1 {HMACSHA1Text(Newtonsoft.Json.JsonConvert.SerializeObject(param), $"{AppSecret}{serviceCode}")}");
+
+                var result = httpClient.SendAsync(request).Result;
+                var resultStr = result.Content.ReadAsStringAsync().Result;
+
+                if (!result.IsSuccessStatusCode)
+                {
+                    throw new Exception(resultStr);
+                }
+                return resultStr;
+            }
+        }
+    }
+}
