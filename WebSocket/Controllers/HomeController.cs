c9e00a32a28db6aab3a4bbbfb796f71fd90d5203
chengshen.wang <805967524@qq.com>
2020-10-22 18:53:32 +0800

    WebSocket

---
+++ WebSocket/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebSocket.Models;
+
+namespace WebSocket.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// dotnet WebSocket.dll  --urls="http://*:6001" --ip="127.0.0.1" --port=6001 --weight=1
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// ！！！注意：在Configure方法中添加 app.UseWebSockets();
+        /// </summary>
+        public async void MyWebSocket(string name)
+        {
+            if (base.HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                System.Net.WebSockets.WebSocket socket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+                //保存起来
+                SocketManger.AddSocket(name, Guid.NewGuid().ToString(), socket);
+
+                CancellationToken token = new CancellationToken();
+
+                while (socket.State == WebSocketState.Open)
+                {
+                    ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[8192]);
+                    WebSocketReceiveResult result = await socket.ReceiveAsync(buffer, token);
+
+                    string userMessage = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+
+                    if (!string.IsNullOrEmpty(userMessage))
+                    {
+                        if (result.MessageType == WebSocketMessageType.Close)
+                        {
+
+                        }
+                        else
+                        {
+                            SocketManger.SendOne(userMessage, token);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                await HttpContext.Response.WriteAsync("我不处理");
+            }
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebSocket/Models/WebSocketMiddleWare.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocket.Models
+{
+    public class WebSocketMiddleWare
+    {
+        /*
+         自定义中间件：
+         1.具有ResquestDelegate参数的构造函数
+         2.具有名为InvokeAsync或Invoke的方法，必须满足两个条件
+            a.返回task
+            b.类型为httpContext的参数
+        */
+
+        private RequestDelegate _next;
+        public WebSocketMiddleWare(RequestDelegate resquestDelegate)
+        {
+            this._next = resquestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            if (httpContext.WebSockets.IsWebSocketRequest)
+            {
+                System.Net.WebSockets.WebSocket socket = await httpContext.WebSockets.AcceptWebSocketAsync();
+
+                ArraySegment<byte> arraySegment = new ArraySegment<byte>(Encoding.UTF8.GetBytes("服务器向客户端发消息"));
+
+                var cancellationToken = new CancellationToken();
+                await socket.SendAsync(arraySegment, System.Net.WebSockets.WebSocketMessageType.Text, true, cancellationToken);
+
+                await _next(httpContext);
+            }
+            else
+            {
+                await httpContext.Response.WriteAsync("我不处理");
+            }
+
+        }
+    }
+
+    public static class WebSocketMiddleWareExstion
+    {
+        public static IApplicationBuilder UseWebSocket(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<WebSocketMiddleWare>();
+        }
+    }
+
+}
