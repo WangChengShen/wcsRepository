011fa34b10381083cc5fb9fb5c6a1322c1b83302
chengshen.wang <805967524@qq.com>
2020-11-24 18:09:48 +0800

    Lucene.Net

---
+++ LuceneDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LuceneDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LuceneDemo/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuceneDemo.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
