61faf7bc84586d46ee7f63a1160a2a6312a258c7
王承申 <chengshen.wang@insupai.com>
2021-01-15 13:53:55 +0800

    redis mq

---
+++ Seckill/Demo/RedisMq.cs
@@ -0,0 +1,97 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisDemo.Demo
+{
+    public class RedisMq
+    {
+		/// <summary>
+		/// 发布消息
+		/// </summary>
+		public static void PublishMsg()
+		{
+			try
+			{
+				//创建一个公众号--创建一个主题
+				Console.WriteLine("发布服务");
+				IRedisClientsManager redisClientManager = new PooledRedisClientManager("127.0.0.1:6379");
+				string topicname = "Send_Log";//发布订阅消息的标识
+				RedisPubSubServer pubSubServer = new RedisPubSubServer(redisClientManager, topicname)
+				{
+					//收到消息事件，发布消息服务可以不注册此事件
+					OnMessage = (channel, msg) =>
+					{
+						Console.WriteLine($"从频道：{channel}上接受到消息：{msg},时间：{DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}");
+						Console.WriteLine("___________________________________________________________________");
+					},
+					OnStart = () =>//服务启动出发事件
+					{
+						Console.WriteLine("发布服务已启动");
+						Console.WriteLine("___________________________________________________________________");
+					},
+					OnStop = () => { Console.WriteLine("发布服务停止"); },
+					OnUnSubscribe = channel => { Console.WriteLine(channel); },
+					OnError = e => { Console.WriteLine(e.Message); },
+					OnFailover = s => { Console.WriteLine(s); },
+				};
+				//接收消息
+				pubSubServer.Start();
+				while (true)
+				{
+					Console.WriteLine("请输入记录的日志");
+					string message = Console.ReadLine();
+					redisClientManager.GetClient().PublishMessage(topicname, message);
+				}
+			}
+			catch (Exception ex)
+			{ 
+				Console.WriteLine(ex.Message);
+			} 
+		}
+
+		/// <summary>
+		/// 消费者订阅消息
+		/// </summary>
+		public static void SubscriptionMsg()
+        {
+			try
+			{
+				using (RedisClient consumer = new RedisClient("127.0.0.1", 6379))
+				{
+					Console.WriteLine($"创建订阅异常信息数据库记录");
+					var subscription = consumer.CreateSubscription();
+					//接受到消息时
+					subscription.OnMessage = (channel, msg) =>
+					{
+						if (msg != "CTRL:PULSE")
+						{
+							Console.WriteLine($"从频道：{channel}上接受到消息：{msg},时间：{DateTime.Now.ToString("yyyyMMdd HH:mm:sss")}");
+							//Logger.WriteLogByDB(msg);
+							//Console.WriteLine("_________________________________记录成功__________________________________");
+						}
+					};
+					//订阅频道时
+					subscription.OnSubscribe = (channel) =>
+					{
+						Console.WriteLine("订阅客户端：开始订阅" + channel);
+					};
+					//取消订阅频道时
+					subscription.OnUnSubscribe = (a) => { Console.WriteLine("订阅客户端：取消订阅"); };
+
+					//订阅频道
+					string topicname = "Send_Log";
+					subscription.SubscribeToChannels(topicname);
+				}
+			}
+			catch (Exception ex)
+			{ 
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+
+		
+	}
+}
--- Seckill/Program.cs
+++ Seckill/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using RedisDemo.Demo;
 using ServiceStack.Redis;
 using System;
 using System.Threading;
@@ -8,6 +9,10 @@ namespace RedisDemo
 {
     class Program
     {
+        /// <summary>
+        /// dotnet RedisDemo.dll --id=1 --minute=10
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //开始命令行模式，这样可以获取命令行启动服务时输入的参数
@@ -16,22 +21,38 @@ namespace RedisDemo
                   .Build();
 
             //1.秒杀：
-            //string id = config["id"];
-            //int minute = Convert.ToInt32(config["minute"]);
-
-            //Seckill.Seckill1(id, minute);  
-            //Seckill.Seckill2(id, minute);  
-
-            //2.事务
-            //RedisTransaction.TransationDemo();
-            //RedisTransaction.TransationDemo2();
-
-
             string id = config["id"];
             int minute = Convert.ToInt32(config["minute"]);
 
-            //3.分布式锁
-            RedisDistributedLock.Skills(minute,15);
+            Console.WriteLine("请输入您要运行的方法");
+            string inputStr = Console.ReadLine();
+
+            if (inputStr == "Seckill1")
+            {
+                Seckill.Seckill1(id, minute);
+                Seckill.Seckill2(id, minute);
+            }
+            else if (inputStr == "Transaction")
+            {
+                //2.事务
+                RedisTransaction.TransationDemo();
+                RedisTransaction.TransationDemo2();
+            }
+            else if (inputStr == "DistributedLock")
+            {
+                //3.分布式锁
+                RedisDistributedLock.Skills(minute, 15);
+            }
+            else if (inputStr == "PublishMsg") //发布消息
+            {
+                RedisMq.PublishMsg();
+            }
+            else if (inputStr == "SubscriptionMsg") //订阅消息
+            {
+                RedisMq.SubscriptionMsg();
+            }
+
+
 
         }
 
